cd41655d44fa10caa9bb16d55c52a5ae2dc195fd
kaylaknight385 <kaylaknight385@gmail.com>
2026-01-20 01:02:11 -0500

    omg it woooooorked! stop playing with me MWAH!

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 // my function that will handle connecting to the database
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     // if we get here, the connection was successful!
     console.log('omg mongodb connected successfully');
   } catch (error) {
